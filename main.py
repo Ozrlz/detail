463d2afa7fa98fe0ddbe06a46f64c2fb45570c29
ozrlz <oscar.cucei@gmail.com>
2022-03-08 21:40:44 -0600

    [IMP] Change separator

--- main.py
+++ main.py
@@ -3,10 +3,10 @@ from pprint import pprint
 
 categories={}
 
-with open('completo.csv', 'r') as filein:
+with open('filtrado.csv', 'r') as filein:
     for line in filein.readlines():
-        splitted = line.split(',')
-        ammount = float( splitted[3].replace('\"', '').replace('$', '') )
+        splitted = line.split('|')
+        ammount = float( splitted[3].replace('\"', '').replace('$', '').replace(',', '') )
         if not splitted[1] in categories.keys() :
             categories[splitted[1]] = {'Detail':[ammount]}
         else:
